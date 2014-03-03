29147c3db227a41f3e05f15ece966484749ed19a
Brien Colwell <xcolwell@gmail.com>
2014-03-02 18:17:08 -0800

    server: messages and build

---
+++ project_code/server/src/main/java/io/rainbow/commonthread/server/CommonThreadServer.java
@@ -0,0 +1,13 @@
+package io.rainbow.commonthread.server;
+
+public final class CommonThreadServer {
+
+	// TODO
+	// 1. netty speaking proto bufs
+	// 2. list of active connections
+	// 3. sync working on connect
+	// 4. event-driven 
+	public static void main(String[] in) {
+		
+	}
+}
